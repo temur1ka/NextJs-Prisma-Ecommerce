4eb79928700fae2c1c808d51ca4693e8b1e775fe
temuka <temometreveli60@gmail.com>
2024-08-23 21:42:54 +0400

    feat

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Badge } from "@/components/ui/badge";
 import React from "react";
 
@@ -34,5 +35,68 @@ const Navbar = () => {
     </header>
   );
 };
+export default function Nav() {
+  const [open, setOpen] = React.useState(false);
+  const toggleOpen = () => setOpen(!open);
+  return (
+    <>
+      <nav className="">
+        <div className="hidden md:block">
+          <Navbar />
+        </div>
+        <div className="flex justify-end container">
+          <button
+            onClick={toggleOpen}
+            className="md:hidden "
+          >
+            {open ? (
+              <span className="text-3xl z-10 text-white">✕</span>
+            ) : (
+              <span className="text-3xl">☰</span>
+            )}
+          </button>
+        </div>
+      </nav>
+      {open && (
+        <div className="fixed top-0  w-full h-full bg-black opacity-50 z-10">
+          <div className="fixed top-0 left-0 w-full h-full  z-30 text-white">
+          <button
+            onClick={toggleOpen}
+            className="md:hidden flex justify-end container "
+          >
+            {open ? (
+              <span className="text-3xl">✕</span>
+            ) : (
+              <span className="text-3xl">☰</span>
+            )}
+          </button>
+            <ul className="flex flex-col justify-center gap-10">
+              <li>
+                <a href="/" className="font-bold text-2xl">
+                  EcomPrisma™.
+                </a>
+              </li>
+              <li>
+                <a href="/">About</a>
+              </li>
+              <li>
+                <a href="/">Contact</a>
+              </li>
 
-export default Navbar;
+              <li>
+                <a href="/">Search</a>
+              </li>
+              <li>
+                <a href="/">Help</a>
+              </li>
+              <li>
+                <a href="/">My Account</a>
+              </li>
+            </ul>
+            <Badge>0</Badge>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
